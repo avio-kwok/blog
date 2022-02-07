0516c92872220d2160c172fe00d955c836fbf3f4
avio-kwok <84455796+avio-kwok@users.noreply.github.com>
2022-02-07 22:36:39 +0800

    update react set up

--- blogweb/blog/views.py
+++ blogweb/blog/views.py
@@ -1,3 +1,22 @@
+from aiohttp import request
 from django.shortcuts import render
+from .serializers import CatSerializer, BlogSerializer
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework import viewsets
+from .models import category, blog
+
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+    
+
+class blogview(ListAPIView):
+    queryset= blog.objects.all()
+    serializer_blog= BlogSerializer
+
+class catlistview(ListAPIView):
+    queryset= category.objects.all()
+    serializer_blog= CatSerializer
